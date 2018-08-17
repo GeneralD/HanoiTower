59dec635d48311b73281c9714f2e8055079c23dc
yumenosuke-k <yumejustice@gmail.com>
2018-08-17 13:44:20 +0900

    using monad

--- HanoiTower/Program.cs
+++ HanoiTower/Program.cs
@@ -1,21 +1,29 @@
 ﻿using System;
 using System.Collections.Async;
-using System.Threading;
 using Functional;
+using Monad;
+using System.Linq;
 
 namespace HanoiTower {
     class Program {
-        static void Main(string[] args) {
+        private static void Main(string[] args) {
             var hanoi = RecursableFuncExtensions.RecurseMemorised<int, int, int, int, AsyncEnumerable<string>>
             ((next, src, bare, dst, height) => new AsyncEnumerable<string>(async yield => {
-                if (height > 1) foreach (var r in next(src, dst, bare, height - 1).ToEnumerable()) await yield.ReturnAsync(r);
+                if (height > 1)
+                    foreach (var r in next(src, dst, bare, height - 1).ToEnumerable())
+                        await yield.ReturnAsync(r);
                 await yield.ReturnAsync($"object: {height}, tower: {src}->{dst}");
-                if (height > 1) foreach (var r in next(bare, src, dst, height - 1).ToEnumerable()) await yield.ReturnAsync(r);
-            }).Curry()(1)(2)(3);
+                if (height > 1)
+                    foreach (var r in next(bare, src, dst, height - 1).ToEnumerable())
+                        await yield.ReturnAsync(r);
+            })).Curry()(1)(2)(3);
 
-            int h;
-            if (args.Length == 0 || !int.TryParse(args[0], out h)) h = 3;
-            hanoi(h).ForEachAsync(s => Console.WriteLine(s));
+            Option<string> First(string[] ary) => () => ary.FirstOrDefault();
+            Try<int> TryParse(string str) => () => int.Parse(str);
+
+            var monad = First(args).Select(TryParse).Match(v => v, () => 3)();
+
+            hanoi(monad().Value).ForEachAsync(s => Console.WriteLine(s));
         }
     }
 }
