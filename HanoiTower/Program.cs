21602e31316869b1d39eb6878fbac8d2fe2d1234
yumenosuke-k <yumejustice@gmail.com>
2018-08-19 02:08:21 +0900

    [from now] 2018/08/19 02:08:21
    diff --git a/HanoiTower/HanoiTower.csproj b/HanoiTower/HanoiTower.csproj
    index 2c2f4db..640efab 100644
    --- a/HanoiTower/HanoiTower.csproj
    +++ b/HanoiTower/HanoiTower.csproj
    @@ -9,8 +9,8 @@
       </ItemGroup>
       <ItemGroup>
         <PackageReference Include="csharp-monad" Version="1.0.1" />
    -    <PackageReference Include="AsyncEnumerator" Version="2.2.1" />
         <PackageReference Include="CSharp.Curry" Version="1.0.0" />
         <PackageReference Include="Funcursive" Version="1.0.0.1" />
    +    <PackageReference Include="System.Interactive" Version="3.2.0" />
       </ItemGroup>
     </Project>
    \ No newline at end of file
    diff --git a/HanoiTower/Program.cs b/HanoiTower/Program.cs
    index c67bf9a..a778958 100644
    --- a/HanoiTower/Program.cs
    +++ b/HanoiTower/Program.cs
    @@ -1,32 +1,31 @@
     ﻿using System;
    -using System.Collections.Async;
    +using System.Collections.Generic;
     using System.Linq;
     using CSharp.Curry;
     using Funcursive;
     using Monad;
    
     namespace HanoiTower {
    -    class Program {
    +    internal static class Program {
             private static void Main(string[] args) {
    -            var hanoi = FuncR<int, int, int, int, AsyncEnumerable<string>>.Create((src, bare, dst, height, next) =>
    -                new AsyncEnumerable<string>(async yield => {
    -                    if (height > 1)
    -                        foreach (var r in next(src, dst, bare, height - 1).ToEnumerable())
    -                            await yield.ReturnAsync(r);
    -                    await yield.ReturnAsync($"object: {height}, tower: {src}->{dst}");
    -                    if (height > 1)
    -                        foreach (var r in next(bare, src, dst, height - 1).ToEnumerable())
    -                            await yield.ReturnAsync(r);
    -                })).Curry()(1)(2)(3);
    +            var hanoi = FuncR<int, int, int, int, IEnumerable<Tuple<int, int, int>>>
    +                .Create((src, bare, dst, height, next) =>
    +                    EnumerableEx.Create<Tuple<int, int, int>>(async yielder => {
    +                        if (height > 1) foreach (var r in next(src, dst, bare, height - 1)) await yielder.Return(r);
    +                        await yielder.Return(new Tuple<int, int, int>(height, src, dst));
    +                        if (height > 1) foreach (var r in next(bare, src, dst, height - 1)) await yielder.Return(r);
    +                    })).Curry()(1)(2)(3);
    
                 Try<string> First(string[] ary) => () => ary.First();
                 Try<int> TryParse(string str) => () => int.Parse(str);
    
    -            var monad = from v in First(args)
    +            var monad =
    +                from v in First(args)
                     from r in TryParse(v)
                     select r;
    
    -            hanoi(monad.Match(i => i, e => 3)()).ForEachAsync(s => Console.WriteLine(s));
    +            hanoi(monad.Match(i => i, e => 3)())
    +                .ForEach(s => Console.WriteLine($"object: {s.Item1}, tower: {s.Item2}->{s.Item3}"));
             }
         }
     }
    \ No newline at end of file

--- HanoiTower/Program.cs
+++ HanoiTower/Program.cs
@@ -1,32 +1,31 @@
 ﻿using System;
-using System.Collections.Async;
+using System.Collections.Generic;
 using System.Linq;
 using CSharp.Curry;
 using Funcursive;
 using Monad;
 
 namespace HanoiTower {
-    class Program {
+    internal static class Program {
         private static void Main(string[] args) {
-            var hanoi = FuncR<int, int, int, int, AsyncEnumerable<string>>.Create((src, bare, dst, height, next) =>
-                new AsyncEnumerable<string>(async yield => {
-                    if (height > 1)
-                        foreach (var r in next(src, dst, bare, height - 1).ToEnumerable())
-                            await yield.ReturnAsync(r);
-                    await yield.ReturnAsync($"object: {height}, tower: {src}->{dst}");
-                    if (height > 1)
-                        foreach (var r in next(bare, src, dst, height - 1).ToEnumerable())
-                            await yield.ReturnAsync(r);
-                })).Curry()(1)(2)(3);
+            var hanoi = FuncR<int, int, int, int, IEnumerable<Tuple<int, int, int>>>
+                .Create((src, bare, dst, height, next) =>
+                    EnumerableEx.Create<Tuple<int, int, int>>(async yielder => {
+                        if (height > 1) foreach (var r in next(src, dst, bare, height - 1)) await yielder.Return(r);
+                        await yielder.Return(new Tuple<int, int, int>(height, src, dst));
+                        if (height > 1) foreach (var r in next(bare, src, dst, height - 1)) await yielder.Return(r);
+                    })).Curry()(1)(2)(3);
 
             Try<string> First(string[] ary) => () => ary.First();
             Try<int> TryParse(string str) => () => int.Parse(str);
 
-            var monad = from v in First(args)
+            var monad =
+                from v in First(args)
                 from r in TryParse(v)
                 select r;
 
-            hanoi(monad.Match(i => i, e => 3)()).ForEachAsync(s => Console.WriteLine(s));
+            hanoi(monad.Match(i => i, e => 3)())
+                .ForEach(s => Console.WriteLine($"object: {s.Item1}, tower: {s.Item2}->{s.Item3}"));
         }
     }
 }
